481088225446ca1abb349d0c2ca0a813e8aed5fd
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-20 16:24:17 +0200

    Spiel Progress bisher nur das Aquarium geschaftt

--- AquariumSpiel/AquariumSpiel/Program.cs
+++ AquariumSpiel/AquariumSpiel/Program.cs
@@ -10,6 +10,39 @@ namespace AquariumSpiel
     {
         static void Main(string[] args)
         {
+            int x = 20;
+            int y = 50;
+            string[,] aquarium = new string[y, x];
+            for (int j = 0; j < x; j++)
+            {
+                for (int i = 0; i < y; i++)
+                {
+                    if (i == 0 || i == y - 1)
+                    {
+                        aquarium[i, j] = "|";
+                    }
+                    else
+                    {
+                        aquarium[i, j] = " ";
+                    }
+                    if (j == x-1)
+                    {
+                        aquarium[i, j] = "-";
+                    }
+                    
+                }
+            }
+                for (int j = 0; j < x; j++)
+                {
+                    for (int i = 0; i < y; i++)
+                    {
+                        Console.Write(aquarium[i, j]);
+                    }
+                    //\n bedeutet es wird eine neue Zeile angebrochen
+                    Console.Write("\n");
+                }
+                Console.ReadLine();
+            
         }
     }
 }
