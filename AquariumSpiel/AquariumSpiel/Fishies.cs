d58a4a4d801bab3766db2f8ee8fc0266e622f966
Welker, Lukas <lukas.welker@tecalliance.de>
2022-09-21 16:01:46 +0200

    Fortschritt

---
+++ AquariumSpiel/AquariumSpiel/Fishies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquariumSpiel
+{
+    public class Fishies
+    {
+      //Properties
+         public int PositionY { get; set; }
+         public int PositionX { get; set; }
+        public string Shape { get; set; }
+        public string ReverseShape { get; set; }
+        public int ChangeDepthProb { get; set; }
+        public string Direction { get; set; }
+        public int Speed { get; set; }
+
+
+
+
+    }
+}
